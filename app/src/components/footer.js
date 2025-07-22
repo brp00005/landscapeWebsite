a91662cd94d3c366511df316535b226820da0f6c
brp00005 <brp00005@mix.wvu.edu>
2025-07-22 13:39:12 -0400

    fix

---
+++ app/src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="footer-bar">
+      <p>&copy; {currentYear} All Pro Landscaping. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
