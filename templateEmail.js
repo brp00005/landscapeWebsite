ddab1682f0218c9b6371849560ca766cd19b93fc
brp00005 <brp00005@mix.wvu.edu>
2025-10-21 15:16:57 -0400

    add template

---
+++ templateEmail.js
@@ -0,0 +1,35 @@
+
+<div style="font-family: 'Times New Roman', sans-serif, Arial; font-size: 20px">
+
+    <p style="padding-top: 16px; border-top: 1px solid #eaeaea">Hi {{ name }},</p>
+    <p>
+        Thank you for reaching out to us! We have received your request, and we'll follow up shortly. We look foward to doing business with you.
+    </p>
+    <p style="padding-top: 16px; border-top: 1px solid #eaeaea">
+        Best regards,<br />The All Pro Landscaping Team
+        <a href="Cell: 856-373-3455">
+            <Icon.Phone size={30} color="rgba(8, 138, 54)" />
+            <div className="ContactText"> Cell: 856-373-3455</div>
+        </a>
+        <a href="Luke: 856-373-3455">
+            <Icon.Phone size={30} color="rgba(8, 138, 54)" />
+            <div className="ContactText"> Luke: 304-777-9672</div>
+        </a>
+
+        <a href="mailto:allprolandscaping1@hotmail.com" className="ContactsWrapper">
+            <Icon.Mail size={31} color="rgb(8, 138, 54)" />
+            <div className="ContactText"> allprolandscaping1@hotmail.com</div>
+
+            <a href="https://p1m.39d.mytemp.website/">
+                <Icon.Mail size={31} color="rgb(8, 138, 54)" />
+                <div> Website</div>
+
+
+                <a style="text-decoration: none; outline: none" href="https://bing.com" target="_blank">
+                    <img style="height: 32px; vertical-align: middle" height="32px" src="https://png.pngtree.com/png-clipart/20230211/original/pngtree-tree-logo-design-png-image_8952328.png" alt="tree logo" />
+                </a>
+            </a>
+    </p>
+
+    <p style="font-size: 14px;">Please do not reply to this email. Replies to this email address are not monitored.</p>
+</div>
